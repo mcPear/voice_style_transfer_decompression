cced09af49100e784a407f7c89c97f42eae8869f
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2020-01-24 13:35:41 +0100

    use wavenet vocoder

--- voice_conversion/plots.py
+++ voice_conversion/plots.py
@@ -15,17 +15,17 @@ def plot(x,x_trg,result=None):
     print(x.shape, x_trg.shape)
     plt.figure(figsize=(12, 12))
     plt.subplot(3,1,1)
-    librosa.display.specshow(x, y_axis='log', sr=16000)
+    librosa.display.specshow(x, sr=16000)
     plt.colorbar(format='%+2.0f dB')
     plt.title('Log-frequency power spectrogram')
 
     plt.subplot(3,1,2)
-    librosa.display.specshow(x_trg, y_axis='log', sr=16000)
+    librosa.display.specshow(x_trg, sr=16000)
     plt.colorbar(format='%+2.00f dB')
     plt.title('Log-frequency power spectrogram')
 
     plt.subplot(3,1,3)
-    librosa.display.specshow(result, y_axis='log', sr=16000)
+    librosa.display.specshow(result, sr=16000)
     plt.colorbar(format='%+2.00f dB')
     plt.title('Log-frequency power spectrogram')
 
