f1a3eae5771532c2ee73b106b3a4857740be4f93
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2020-01-24 22:13:44 +0100

    add generated samples

--- implementation/plots.py
+++ implementation/plots.py
@@ -17,16 +17,16 @@ def plot(x,x_trg,result=None):
     plt.subplot(3,1,1)
     librosa.display.specshow(x, sr=16000)
     plt.colorbar(format='%+2.0f dB')
-    plt.title('Log-frequency power spectrogram')
+    plt.title('Original utterance spectrogram')
 
     plt.subplot(3,1,2)
     librosa.display.specshow(x_trg, sr=16000)
     plt.colorbar(format='%+2.00f dB')
-    plt.title('Log-frequency power spectrogram')
+    plt.title('Compressed utterance spectrogram')
 
     plt.subplot(3,1,3)
     librosa.display.specshow(result, sr=16000)
     plt.colorbar(format='%+2.00f dB')
-    plt.title('Log-frequency power spectrogram')
+    plt.title('Outpt spectrogram')
 
     plt.show()
